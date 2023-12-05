c1a1087c1c7e0152f89cb44e518182bf03434ebf
Margherita <margherita.deodato14@gmail.com>
2023-12-05 14:49:02 +0100

    commit testing4

---
+++ src/MainTest.java
@@ -0,0 +1,42 @@
+import org.junit.jupiter.api.Test;
+import java.time.OffsetDateTime;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    private final Main test = new Main();
+
+    @Test
+    void anno() {
+        String data = "2025-03-01T13:00:00Z";
+        OffsetDateTime offsetDateTime = OffsetDateTime.parse(data);
+        int result = test.anno(offsetDateTime);
+        assertEquals(2025, result);
+    }
+
+    @Test
+    void mese() {
+        String data = "2023-11-01T13:00:00Z";
+        OffsetDateTime offsetDateTime = OffsetDateTime.parse(data);
+        String result = test.mese(offsetDateTime);
+        assertEquals("NOVEMBER", result);
+    }
+
+    @Test
+    void giorno() {
+        String data = "2023-03-11T13:00:00Z";
+        OffsetDateTime offsetDateTime = OffsetDateTime.parse(data);
+        int result = test.giorno(offsetDateTime);
+        assertEquals(11, result);
+    }
+
+    @Test
+    void giornoSettimana() {
+        String data = "2023-03-01T13:00:00Z";
+        OffsetDateTime offsetDateTime = OffsetDateTime.parse(data);
+        String result = test.giornoSettimana(offsetDateTime);
+        assertEquals("WEDNESDAY", result);
+    }
+}
+
+
+
